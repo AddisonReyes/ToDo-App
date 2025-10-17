eb8510c78d143e84a51bb4e4c7f2e1d456495f29
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2025-10-17 18:08:04 -0400

    Add dotenv support and frontend Dockerfile
    Introduced dotenv for environment variable management in the backend, updated MongoDB and server port configuration to use environment variables, and improved error handling.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,12 @@ import cors from "cors";
 
 const user: string = "admin";
 const password: string = "123456";
-const connectionString = `mongodb://${user}:${password}@mongodb:27017/todo-app?authSource=admin&retryWrites=true&w=majority`;
+
+const mongoPort = process.env.MONGODB_PORT || 27017;
+const url: string = `mongodb://${user}:${password}@mongodb:${mongoPort}/`;
+const db: string = "todo-app";
+const connectionString =
+  url + db + "?authSource=admin&retryWrites=true&w=majority";
 
 const app = express();
 
@@ -20,5 +25,15 @@ mongoose.connect(connectionString);
 
 // Routes
 app.use("/tasks", router);
+
+// Client errors
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ message: "404 - Not Found", status: 404 });
+});
+
+// Server errors
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500).json({ error: err.message, status: 500 });
+});
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,9 @@
+import dotenv from "dotenv";
 import app from "./app.js";
 
-const port: number = 5000;
+dotenv.config();
+
+const port = process.env.BACKEND_PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server listen on http://localhost:${port}`);
